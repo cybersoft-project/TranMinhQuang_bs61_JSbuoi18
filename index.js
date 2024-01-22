113b9ed05c83cf675a9108b8067d542ad1cb6fa7
minhquang3225 <60904218+minhquang3225@users.noreply.github.com>
2024-01-22 17:16:17 +0700

    finish

--- index.js
+++ index.js
@@ -1,175 +1,206 @@
-var divKetQua1 = document.getElementById('ketQua1');
-var divKetQua2 = document.getElementById('ketQua2');
-var divKetQua3 = document.getElementById('ketQua3');
-var divKetQua4 = document.getElementById('ketQua4');
+let btn = document.querySelector('.btn-success');
+let btnBai6 = document.querySelector('.btnBai6');
+let btnBai7 = document.querySelector('.btnBai7');
+let divArr = document.querySelector('.render_arr');
+let divBai1 = document.getElementById('kq1'),
+    divBai2 = document.getElementById('kq2'),
+    divBai3 = document.getElementById('kq3'),
+    divBai4 = document.getElementById('kq4'),
+    divBai5 = document.getElementById('kq5'),
+    divBai6 = document.getElementById('kq6'),
+    divBai7 = document.getElementById('kq7'),
+    divBai8 = document.getElementById('kq8'),
+    divBai9 = document.getElementById('kq9'),
+    divBai10 = document.getElementById('kq10');
 
 
-function baiTap1() {
-    var diemChuan = document.getElementById('txt-diemChuan').value * 1;
-    var vung = document.getElementById('txt-vung').value;
-    var doiTuong = document.getElementById('txt-doiTuong').value;
-    var diem1 = document.getElementById('txt-diem1').value * 1;
-    var diem2 = document.getElementById('txt-diem2').value * 1;
-    var diem3 = document.getElementById('txt-diem3').value * 1;
-    var diemKhuVuc, diemDoiTuong;
-    switch (vung) {
-        case "A":
-            diemKhuVuc = 2;
-            break;
-        case "B":
-            diemKhuVuc = 1;
-            break;
-        case "C":
-            diemKhuVuc = 0.5;
-            break;
 
-        default:
-            diemKhuVuc = 0;
-            break;
-    }
-    switch (doiTuong) {
-        case "1":
-            diemDoiTuong = 2.5;
-            break;
-        case "2":
-            diemDoiTuong = 1.5;
-            break;
-        case "3":
-            diemDoiTuong = 1;
-            break;
 
-        default:
-            diemDoiTuong = 0;
-            break;
-    }
-
-    var diemTongKet = diem1 + diem2 + diem3 + diemKhuVuc + diemDoiTuong;
-
-    if (diemTongKet >= diemChuan && diem1 != 0 && diem2 != 0 && diem3 != 0) {
-        divKetQua1.innerHTML = `Chuc mung ban da dau 🤩 voi so diem la: ${diemTongKet}`
-    }
-    else {
-        divKetQua1.innerHTML = `Rat tiec 😥 ban da rot, so diem cua ban la ${diemTongKet}`
-    }
-
+var arrSoN = [];
+btnBai6.onclick = function () {
+    giaiBai6();
+    giaiBaiTap();
+    showKetQua();
 }
-function baiTap2() {
-    var tenKhachHang = document.getElementById('txt-tenKhachHang').value;
-    var soDien = document.getElementById('txt-soDien').value * 1;
-    var soTien = 0;
-
-    if (soDien <= 50) {
-        soTien = soDien * 500;
-    }
-    else if (soDien > 50 && soDien <= 100) {
-        soTien = 50 * 500 + (soDien - 50) * 650;
-    }
-    else if (soDien > 100 && soDien <= 200) {
-        soTien = 50 * 500 + 50 * 650 + (soDien - 100) * 850;
-    }
-    else if (soDien > 200 && soDien <= 350) {
-        soTien = 50 * 500 + 50 * 650 + 100 * 850 + (soDien - 200) * 1100;
-    }
-    else {
-        soTien = 50 * 500 + 50 * 650 + 100 * 850 + 150 * 110 + (soDien - 350) * 1300;
-    }
-    var formatTien = soTien.toLocaleString('vi', {
-        style: 'currency',
-        currency: 'VND'
-    })
-    divKetQua2.innerHTML = `Khach hang ${tenKhachHang} phai tra: ${formatTien}`;
+btnBai7.onclick = function () {
+    giaiBai7();
+    giaiBaiTap();
+    showKetQua();
 }
+btn.onclick = function () {
+    themSoNVaoMang();
+    giaiBaiTap();
+    showKetQua();
+    clearInput();
+};
+function themSoNVaoMang() {
+    let soN = document.getElementById('soN').value * 1;
+    arrSoN.push(soN);
+}
+function showKetQua() {
+    divArr.innerHTML = arrSoN;
+}
+function giaiBaiTap() {
+    giaiBai1();
+    giaiBai2();
+    giaiBai3();
+    giaiBai4();
+    giaiBai5();
+    giaiBai8();
+    giaiBai9();
+    giaiBai10();
+}
+function giaiBai1() {
+    let tong = 0;
+    for (let i = 0; i < arrSoN.length; i++) {
+        if (arrSoN[i] > 0) {
+            tong += arrSoN[i];
+        }
+        divBai1.innerHTML = `Bai 1: Tong cac so duong trong mang la: ${tong}`
+    }
+}
+function giaiBai2() {
+    let dem = 0;
+    for (let i = 0; i < arrSoN.length; i++) {
+        if (arrSoN[i] > 0) {
+            dem++;
+        }
 
-function baiTap3() {
-    var hoTen = document.getElementById('txt-hoTen').value
-    var tongThuNhap = document.getElementById('txt-tongThuNhap').value * 1;
-    var soNguoiPhuThuoc = document.getElementById('txt-soNguoiPhuThuoc').value * 1;
-    var thueSuat = 0;
-    var tienThue = 0;
-
-    if (tongThuNhap <= 60000000) {
-        thueSuat = 5 / 100;
     }
-    else if (tongThuNhap > 60000000 && tongThuNhap <= 120000000) {
-        thueSuat = 10 / 100
+    divBai2.innerHTML = `Bai 2: mang co ${dem} so duong trong mang`
+}
+function giaiBai3() {
+    let min = arrSoN[0];
+    for (let o = 1; o < arrSoN.length; o++) {
+        if (min > arrSoN[o]) {
+            min = arrSoN[o];
+        }
     }
-    else if (tongThuNhap > 120000000 && tongThuNhap <= 210000000) {
-        thueSuat = 15 / 100
-    }
-    else if (tongThuNhap > 2100000000 && tongThuNhap <= 384000000) {
-        thueSuat = 20 / 100
-    }
-    else if (tongThuNhap > 3840000000 && tongThuNhap <= 624000000) {
-        thueSuat = 25 / 100
-    }
-    else if (tongThuNhap > 624000000 && tongThuNhap <= 960000000) {
-        thueSuat = 30 / 100
-    }
-    else {
-        thueSuat = 35 / 100
-    }
-
-    tienThue = thueSuat * (tongThuNhap - 4000000 - soNguoiPhuThuoc * 1600000)
-    var formatTienThue = tienThue.toLocaleString('vi', {
-        style: "currency",
-        currency: 'VND'
+    divBai3.innerHTML = `Bai 3: gia tri nho nhat cua mang la: ${min}`
+}
+function giaiBai4() {
+    let sapXepArr = arrSoN.slice(0).sort(function (a, b) {
+        return a - b;
     })
 
-
-    divKetQua3.innerHTML = `Cong dan ${hoTen} phai dong: ${formatTienThue}`
-}
-
-// bai 4
-var divKetNoi = document.querySelector('.ketNoi');
-var divLoaiKhachHang = document.getElementById('txt-loaiKhachHang')
-var loaiKhachHang 
-
-divLoaiKhachHang.onchange = function () {
-    loaiKhachHang = document.getElementById('txt-loaiKhachHang').value*1;
-    
-    if (loaiKhachHang == 1) {
-        divKetNoi.style.display="block";
+    let max = sapXepArr[sapXepArr.length - 1];
+    if (max >= 0) {
+        divBai4.innerHTML = `Bai 4: so duong lon nhat trong mang la: ${max}`
     }
     else {
-        divKetNoi.style.display='none';
+        divBai4.innerHTML = "Bai 4: mang khong co so duong"
     }
+
 }
-function baiTap4() {
-
-   
-    var soKetNoi = document.getElementById('txt-soKetNoi').value * 1;
-    var maKhachHang = document.getElementById('txt-maKhachHang').value;
-    var soKenh = document.getElementById('txt-soKenh').value*1;
-    var phiHoaDon = 0, phiDichVu = 0, phiThueKenh = 0;
-    var tongTien = 0;
-console.log(loaiKhachHang);
-
-    switch (loaiKhachHang) {
-        case 0:
-            phiHoaDon = 4.5;
-            phiDichVu = 20.5
-            phiThueKenh = 7.5*soKenh
+function giaiBai5() {
+    let soChanCuoiCung = -1;
+    for (let i = arrSoN.length; i >= 0; i--) {
+        if (arrSoN[i] % 2 == 0) {
+            soChanCuoiCung = arrSoN[i];
             break;
-    
-        case 1:
-            phiHoaDon = 15;
-            phiDichVu = soKetNoi<10?75:(75+(soKetNoi-10)*5)
-            phiThueKenh = 50*soKenh
-            break;
+        }
     }
-    
-    tongTien = phiHoaDon + phiDichVu + phiThueKenh;
+    if (soChanCuoiCung != -1) {
+        divBai5.innerHTML = `Bai 5: So chan cuoi cung trong mang: ${soChanCuoiCung}`;
+    }
+    else {
+        divBai5.innerHTML = `Bai 5: Khong co so chan`;
+    }
+
+}
+function giaiBai6() {
+    let index1 = document.getElementById('index1').value * 1,
+        index2 = document.getElementById('index2').value * 1;
 
 
-    var formatTien = tongTien.toLocaleString('vi', {
-        style: 'currency',
-        currency: 'USD',
+    if (index1 && index2) {
+        let value1 = arrSoN[index1];
+        let value2 = arrSoN[index2]
+        arrSoN[index1] = value2;
+        arrSoN[index2] = value1;
+        console.log(arrSoN)
+    }
+    else {
+        alert("da chuyen doi");
+    }
+
+}
+function giaiBai7() {
+    arrSoN.sort(function (a, b) {
+        return a - b;
     })
-    divKetQua4.innerHTML = `Ma khach hang: ${maKhachHang} <br> So tien can thanh toan: ${formatTien}`
+    showKetQua();
+    alert("da sap xep")
+}
+function giaiBai8() {
+    for (let i = 0; i < arrSoN.length; i++) {
+        const soN = arrSoN[i];
+        let isNguyenTo = false;
+        if (soN > 1) {
+            for (let z = 2; z < soN; z++) {
+                if (soN % z == 0)
+                    break;
+                isNguyenTo = true;
+            }
+        }
+        if (isNguyenTo) {
+            divBai8.innerHTML = `Bai 8: so nguyen to dau tien trong mang la so: ${soN}`
+            break;
+        }
+        else {
+            divBai8.innerHTML = "Bai 8: day khong co so nguyen to"
+        }
+    }
+}
+function giaiBai9() {
+    var count = 0;
+    for (let i = 0; i < arrSoN.length; i++) {
+        const soN = arrSoN[i];
+        if (soN % 1 == 0) {
+            count++;
+        }
+    }
+    divBai9.innerHTML = `Bai 9: So phan tu nguyen trong mang la: ${count}`
+}
+function giaiBai10() {
+    var demSoAm = 0, demSoDuong = 0, content
+    for (let i = 0; i < arrSoN.length; i++) {
+        const soN = arrSoN[i];
+        if (soN > 0) {
+            demSoDuong++
+        }
+        if (soN < 0) {
+            demSoAm++;
+        }
+    }
+    if (demSoDuong>demSoAm) {
+        content = 'mang co so duong nhieu hon so am'
+    }
+    else if(demSoDuong<demSoAm){
+        content = 'mang co so am nhieu hon so duong'
+    }
+    else{
+        content = 'mang co so am va so duong bang nhau'
+    }
+    divBai10.innerHTML='Bai 10: '+content;
+}
+
+function clearInput() {
+    document.getElementById('soN').value = '';
+    document.getElementById('soN').focus();
 }
 
 
-document.getElementById('btnBai1').onclick = baiTap1;
-document.getElementById('btnBai2').onclick = baiTap2;
-document.getElementById('btnBai3').onclick = baiTap3;
-document.getElementById('btnBai4').onclick = baiTap4;
+
+
+
+
+
+
+
+
+
+
+
+
+
